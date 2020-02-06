449be6a03ceb2e273b2428efc034045b6134eeab
antoniofregoso <antonio.fregoso.mx@gmail.com>
2020-02-05 23:11:32 -0600

    [FOX]

--- paintball_reservation/static/src/js/paintball_zone_summary.js
+++ paintball_reservation/static/src/js/paintball_zone_summary.js
@@ -1,4 +1,4 @@
-odoo.define('hotel_reservation.hotel_room_summary', function (require) {
+odoo.define('paintball_reservation.paintball_zone_summary', function (require) {
 'use strict';
 
 var core = require('web.core');
@@ -23,17 +23,17 @@ var MyWidget = FieldText.extend({
             date_to: false,
             date_from: false,
             summary_header: false,
-            room_summary: false,
+            zone_summary: false,
         });
         this.set({"summary_header": py.eval(this.recordData.summary_header)});
-        this.set({"room_summary":py.eval(this.recordData.room_summary )});
+        this.set({"zone_summary":py.eval(this.recordData.zone_summary )});
     },
     start: function() {
         var self = this;
         if (self.setting)
             return;
         
-        if (! this.get("summary_header") || ! this.get("room_summary"))
+        if (! this.get("summary_header") || ! this.get("zone_summary"))
                return
                
         this.renderElement();
@@ -43,7 +43,7 @@ var MyWidget = FieldText.extend({
          FormView.ReinitializeWidgetMixin.initialize_field.call(this);
          var self = this;
          self.on("change:summary_header", self, self.start);
-         self.on("change:room_summary", self, self.start);
+         self.on("change:zone_summary", self, self.start);
      },
      view_loading: function(r) {
          return this.load_form(r);
@@ -54,29 +54,29 @@ var MyWidget = FieldText.extend({
          this.$el.find(".table_free").bind("click", function(event){
              self.do_action({
                      type: 'ir.actions.act_window',
-                     res_model: "quick.room.reservation",
+                     res_model: "quick.zone.reservation",
                      views: [[false, 'form']],
                      target: 'new',
-                     context: {"room_id": $(this).attr("data"), 'date': $(this).attr("date"), 'default_adults': 1},
+                     context: {"zone_id": $(this).attr("data"), 'date': $(this).attr("date"), 'default_adults': 1},
              });
          });
      },
      renderElement: function() {
          this._super();
-         this.$el.html(QWeb.render("RoomSummary", {widget: this}));
+         this.$el.html(QWeb.render("ZoneSummary", {widget: this}));
     },
     _onFieldChanged: function (event) {
     	this._super();
         this.lastChangeEvent = event;
     	this.set({"summary_header": py.eval(this.recordData.summary_header)});
-        this.set({"room_summary":py.eval(this.recordData.room_summary)});
+        this.set({"zone_summary":py.eval(this.recordData.zone_summary)});
         this.renderElement();
         this.view_loading();
     },
 });
 
 registry.add(
-    'Room_Reservation', MyWidget
+    'Zone_Reservation', MyWidget
 );
 return MyWidget
 });
